53c88555c1cf216a8f62f32110369bfad1e8dafa
Muhammad Zaka ILLAHi <babrumbabrum12@gmail.com>
2026-02-21 00:30:18 +0500

    Integer comparison via if-else
    ". The code asks for two numbers and uses logic blocks (if, else if, and else ) statements to compare two numbers and print the correct relationship between them."

---
+++ IfElse_ComparisonLogic.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a=0;
+	int b=0;
+	
+	cout<<"Enter first number";
+	cin>>a;
+	cout<<"Enter first number";
+	cin>>b;
+	
+	if(a>b)						
+	{cout<<"a is greater than b";
+	}
+	
+	else if(a<b)
+	{
+	cout<<"a is less than b";	
+	}
+	else{
+	cout<<"a and b are equal";
+	}
+	
+	
+}
